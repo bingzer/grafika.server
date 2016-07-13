9ed530449b7397e6bcea00a14ffad655d8c363ce
Ricky <ricky@bingzer.com>
2016-07-12 20:45:10 -0400

    mongoose

--- server/configs/mongoose.ts
+++ server/configs/mongoose.ts
@@ -1,10 +1,11 @@
 import * as winston from 'winston';
 import * as config from '../configs/config';
 import * as restful from '../libs/restful';
+import * as mongoose from 'mongoose';
 
 export function initialize() {
     winston.debug('Connecting to mongodb');
-    restful.mongoose.connect(config.setting.$server.$databaseUrl, (err) => {
+    mongoose.connect(config.setting.$server.$databaseUrl, (err) => {
         if (!err) winston.info('mongodb [OK]');
         else {
             winston.error('mongodb [FAILED]');
