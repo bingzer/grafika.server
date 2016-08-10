446f1707ff687a883857a7eb819a271698dd7357
Ricky <ricky@bingzer.com>
2016-08-09 20:42:13 -0400

    update

--- client/app/app-config.ts
+++ client/app/app-config.ts
@@ -1,7 +1,7 @@
 module GrafikaApp {
     export class AppConfig {
         appTitle: string = 'Grafika';
-        appVersion: string = '0.9.11-0';
+        appVersion: string = '0.9.12-0';
         baseUrl: string = ''; // injected by app-controller
         defaultBaseUrl: string = 'https://grafika.herokuapp.com/';  // default url if baseUrl is localhost
         apiBaseUrl: string = '/api/';  // local
