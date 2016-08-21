6b57f6ded379ff083ff26979556278a69e72d3ff
Ricky <ricky@bingzer.com>
2016-08-21 08:03:44 -0400

    v0.9.19-9

--- client/app/app-config.js
+++ client/app/app-config.js
@@ -3,8 +3,8 @@ var GrafikaApp;
     var AppConfig = (function () {
         function AppConfig() {
             this.appTitle = 'Grafika';
-            this.appVersion = '0.9.19-6';
-            this.appBuildTimestamp = 'Sun Aug 21 2016 07:49:28 GMT-0400 (Eastern Daylight Time)';
+            this.appVersion = '0.9.19-9';
+            this.appBuildTimestamp = 'Sun Aug 21 2016 08:02:41 GMT-0400 (Eastern Daylight Time)';
             this.baseUrl = '';
             this.defaultBaseUrl = 'https://grafika.herokuapp.com/';
             this.apiBaseUrl = 'api/';
