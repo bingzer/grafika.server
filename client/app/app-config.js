08e119d66ea1934b9fd298d733a3debbf7871e97
Ricky <ricky@bingzer.com>
2016-10-05 18:48:32 -0400

    v0.9.19-41

--- client/app/app-config.js
+++ client/app/app-config.js
@@ -3,8 +3,8 @@ var GrafikaApp;
     var AppConfig = (function () {
         function AppConfig() {
             this.appTitle = 'Grafika';
-            this.appVersion = '0.9.19-40';
-            this.appBuildTimestamp = 'Wed Oct 05 2016 18:02:13 GMT-0400 (Eastern Daylight Time)';
+            this.appVersion = '0.9.19-41';
+            this.appBuildTimestamp = 'Wed Oct 05 2016 18:47:02 GMT-0400 (Eastern Daylight Time)';
             this.baseUrl = '';
             this.defaultBaseUrl = 'https://grafika.herokuapp.com/';
             this.apiBaseUrl = 'api/';
--- client/app/app-config.ts
+++ client/app/app-config.ts
@@ -1,8 +1,8 @@
 module GrafikaApp {
     export class AppConfig {
         appTitle: string = 'Grafika';
-        appVersion: string = '0.9.19-40'; // injected by grunt
-        appBuildTimestamp: string = 'Wed Oct 05 2016 18:02:13 GMT-0400 (Eastern Daylight Time)'; // injected by grunt
+        appVersion: string = '0.9.19-41'; // injected by grunt
+        appBuildTimestamp: string = 'Wed Oct 05 2016 18:47:02 GMT-0400 (Eastern Daylight Time)'; // injected by grunt
         baseUrl: string = ''; // injected by app-controller
         defaultBaseUrl: string = 'https://grafika.herokuapp.com/';  // default url if baseUrl is localhost
         apiBaseUrl: string = 'api/';  // local
