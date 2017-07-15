ff14964cc2482eeaae1b293cd992bf31ed20ede0
Ricky <ricky@bingzer.com>
2017-07-15 10:24:27 -0400

    More ui stuffs

--- Grafika.Utilities/StringExtensions.cs
+++ Grafika.Utilities/StringExtensions.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Grafika.Utilities
 {
     public static class StringExtensions
     {
+        public static bool IsNullOrEmptyOrWhitespace(this string any)
+        {
+            return string.IsNullOrEmpty(any) || string.IsNullOrWhiteSpace(any);
+        }
+
+        public static string SafeTrim(this string any)
+        {
+            if (string.IsNullOrEmpty(any)) return null;
+            return any.Trim();
+        }
+
         public static bool EqualsIgnoreCase(this string any, string compare)
         {
             if (any == null && compare == null) return true;
