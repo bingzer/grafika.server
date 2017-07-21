0d9bcc91a5d44767f771ba2c2fed264d101b221c
Ricky Tobing <ricky@bingzer.com>
2017-07-21 16:37:07 -0400

    Fix more semantics

---
+++ Grafika.WebSite/ViewModels/PageViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Grafika.WebSite.ViewModels
+{
+    public class PageViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        // optional can be blank
+        public string ThumbnailUrl { get; set; }
+
+        // TODO: MOVE ME
+        public string FacebookAppId => "1742692282614357";
+        public string TwitterSiteId => "791028721138401281";
+        public string TwitterHandle => "@grafikapp";
+        public string SiteName => "Grafika";
+        public string SiteUrl => "https://grafika.bingzer.com";
+    }
+}
