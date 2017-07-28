0778bcc061c36ea5b97f345a34c528aaeaf27d0a
Ricky <ricky@bingzer.com>
2017-07-27 22:08:00 -0400

    latest again

--- gulpfile.js
+++ gulpfile.js
@@ -113,13 +113,11 @@ gulp.task('copy-fonts', function () {
 gulp.task('install', function (callback) {
     runSequence('clean', 'bower', 'typings', 'scripts', 'styles', 'minify', callback);
 });
-gulp.task('styles', ['less', 'copy-fonts'], function (callback) {
-    console.log('-- Done Running task styles');
-    callback();
+gulp.task('styles', function (callback) {
+    runSequence('less', 'copy-fonts', callback);
 });
-gulp.task('scripts', ['ts', 'copy-js'], function (callback) {
-    console.log('-- Done Running task scripts');
-    callback();
+gulp.task('scripts', function (callback) {
+    runSequence('ts', 'copy-js', callback);
 });
 gulp.task('minify', function(callback) {
     runSequence('min:js', 'min:css', callback);
@@ -136,163 +134,144 @@ gulp.task('min:js', function (callback) {
 });
 
 gulp.task('min:js:GrafikaApp.Bundle.Site', function (callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/js/bootbox.js',
-            'Grafika.WebSite/wwwroot/js/scrollreveal.js',
-            'Grafika.WebSite/wwwroot/js/grafika.js',
-            'Grafika.WebSite/wwwroot/js/GrafikaApp.js',
-            'Grafika.WebSite/wwwroot/js/GrafikaApp.Partials.js',
-            'Grafika.WebSite/wwwroot/js/GrafikaApp.Dialog.js',
-            'Grafika.WebSite/wwwroot/js/GrafikaApp.Player.js',
-        ]),
-        concat('GrafikaApp.Bundle.Site.min.js'),
-        uglify(),
-        gulp.dest('Grafika.WebSite/wwwroot/js')
-    ], callback)
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/js/bootbox.js',
+        'Grafika.WebSite/wwwroot/js/scrollreveal.js',
+        'Grafika.WebSite/wwwroot/js/grafika.js',
+        'Grafika.WebSite/wwwroot/js/GrafikaApp.js',
+        'Grafika.WebSite/wwwroot/js/GrafikaApp.Partials.js',
+        'Grafika.WebSite/wwwroot/js/GrafikaApp.Dialog.js',
+        'Grafika.WebSite/wwwroot/js/GrafikaApp.Player.js',
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Site.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/js'));
 });
 
 gulp.task('min:js:GrafikaApp.Bundle.Home', function (callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/js/jquery.easing.js',
-            'Grafika.WebSite/wwwroot/js/jquery.magnific-popup.js',
-            'Grafika.WebSite/wwwroot/js/GrafikaApp.Home.js',
-        ]),
-        concat('GrafikaApp.Bundle.Home.min.js'),
-        uglify(),
-        gulp.dest('Grafika.WebSite/wwwroot/js')
-    ], callback);
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/js/jquery.easing.js',
+        'Grafika.WebSite/wwwroot/js/jquery.magnific-popup.js',
+        'Grafika.WebSite/wwwroot/js/GrafikaApp.Home.js',
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Home.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/js'));
 });
 
-gulp.task('min:js:GrafikaApp.Bundle.Drawing', function(callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/js/spectrum.js',
-            'Grafika.WebSite/wwwroot/js/angular.js',
-            'Grafika.WebSite/wwwroot/js/angular-animate.js',
-            'Grafika.WebSite/wwwroot/js/angular-aria.js',
-            'Grafika.WebSite/wwwroot/js/angular-messages.js',
-            'Grafika.WebSite/wwwroot/js/angular-touch.js',
-            'Grafika.WebSite/wwwroot/js/angular-cookies.js',
-            'Grafika.WebSite/wwwroot/js/angular-sanitize.js',
-            'Grafika.WebSite/wwwroot/js/angular-material.js',
-            'Grafika.WebSite/wwwroot/js/angular-ui-router.js',
-            'Grafika.WebSite/wwwroot/js/angular-jwt.js',
-            'Grafika.WebSite/wwwroot/js/angular-spectrum-colorpicker.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Theme.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Base.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppCommon.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppAuthInterceptor.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppHttpInterceptor.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppRoutes.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Filters.CapitalizeFirstLetter.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Directives.ContextMenuDirective.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Directives.ImageUploaderDirective.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.BaseService.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.AnimationService.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.ApiService.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.AuthService.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.FrameService.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.ResourceService.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.BaseController.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.AuthController.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.BaseAnimationController.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.DialogController.js',
-            'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.DrawingController.js',
-            'Grafika.WebSite/wwwroot/js/GrafikaApp.Drawing.js'
-        ]),
-        sourcemaps.init(),
-        concat('GrafikaApp.Bundle.Drawing.min.js'),
-        uglify({ mangle: false }),
-        sourcemaps.write('./'),
-        gulp.dest('Grafika.WebSite/wwwroot/js')
-    ], callback)
+gulp.task('min:js:GrafikaApp.Bundle.Drawing', function (callback) {
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/js/spectrum.js',
+        'Grafika.WebSite/wwwroot/js/angular.js',
+        'Grafika.WebSite/wwwroot/js/angular-animate.js',
+        'Grafika.WebSite/wwwroot/js/angular-aria.js',
+        'Grafika.WebSite/wwwroot/js/angular-messages.js',
+        'Grafika.WebSite/wwwroot/js/angular-touch.js',
+        'Grafika.WebSite/wwwroot/js/angular-cookies.js',
+        'Grafika.WebSite/wwwroot/js/angular-sanitize.js',
+        'Grafika.WebSite/wwwroot/js/angular-material.js',
+        'Grafika.WebSite/wwwroot/js/angular-ui-router.js',
+        'Grafika.WebSite/wwwroot/js/angular-jwt.js',
+        'Grafika.WebSite/wwwroot/js/angular-spectrum-colorpicker.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Theme.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Base.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppCommon.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppAuthInterceptor.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppHttpInterceptor.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.AppRoutes.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Filters.CapitalizeFirstLetter.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Directives.ContextMenuDirective.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Directives.ImageUploaderDirective.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.BaseService.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.AnimationService.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.ApiService.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.AuthService.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.FrameService.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Services.ResourceService.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.BaseController.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.AuthController.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.BaseAnimationController.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.DialogController.js',
+        'Grafika.WebSite/wwwroot/js/drawing/GrafikaApp.Drawing.Controllers.DrawingController.js',
+        'Grafika.WebSite/wwwroot/js/GrafikaApp.Drawing.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('GrafikaApp.Bundle.Drawing.min.js'))
+    .pipe(uglify({ mangle: false }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/js'));
 });
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-gulp.task('min:css', [
-    'min:css:GrafikaApp.Bundle.Site',
-    'min:css:GrafikaApp.Bundle.Home',
-    'min:css:GrafikaApp.Bundle.Login',
-    'min:css:GrafikaApp.Bundle.Drawing',
-    'min:css:GrafikaApp.Bundle.Animation.List',
-    'min:css:GrafikaApp.Bundle.Animation.Detail'
-], function (callback) {
-    console.log('-- Done Running min:css');
-    callback();
+gulp.task('min:css', function (callback) {
+    runSequence(
+        'min:css:GrafikaApp.Bundle.Site',
+        'min:css:GrafikaApp.Bundle.Home',
+        'min:css:GrafikaApp.Bundle.Login',
+        'min:css:GrafikaApp.Bundle.Drawing',
+        'min:css:GrafikaApp.Bundle.Animation.List',
+        'min:css:GrafikaApp.Bundle.Animation.Detail',
+        callback);
 });
 
 gulp.task('min:css:GrafikaApp.Bundle.Site', function (callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/css/bootstrap.css',
-            'Grafika.WebSite/wwwroot/css/font-awesome.css',
-            'Grafika.WebSite/wwwroot/css/bootstrap-xl.css',
-            'Grafika.WebSite/wwwroot/css/GrafikaApp.css',
-            'Grafika.WebSite/wwwroot/css/GrafikaApp.Player.css',
-        ]),
-        concat('GrafikaApp.Bundle.Site.min.css'),
-        cleanCSS(),
-        gulp.dest('Grafika.WebSite/wwwroot/css')
-    ], callback);
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/css/bootstrap.css',
+        'Grafika.WebSite/wwwroot/css/font-awesome.css',
+        'Grafika.WebSite/wwwroot/css/bootstrap-xl.css',
+        'Grafika.WebSite/wwwroot/css/GrafikaApp.css',
+        'Grafika.WebSite/wwwroot/css/GrafikaApp.Player.css',
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Site.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/css'));
 });
 
 gulp.task('min:css:GrafikaApp.Bundle.Home', function (callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/css/magnific-popup.css',
-            'Grafika.WebSite/wwwroot/css/GrafikaApp.Home.css'
-        ]),
-        concat('GrafikaApp.Bundle.Home.min.css'),
-        cleanCSS(),
-        gulp.dest('Grafika.WebSite/wwwroot/css')
-    ], callback);
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/css/magnific-popup.css',
+        'Grafika.WebSite/wwwroot/css/GrafikaApp.Home.css'
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Home.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/css'));
 });
 
 gulp.task('min:css:GrafikaApp.Bundle.Login', function(callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/css/GrafikaApp.Login.css'
-        ]),
-        concat('GrafikaApp.Bundle.Login.min.css'),
-        cleanCSS(),
-        gulp.dest('Grafika.WebSite/wwwroot/css')
-    ], callback);
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/css/GrafikaApp.Login.css'
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Login.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/css'));
 });
 
 gulp.task('min:css:GrafikaApp.Bundle.Animation.List', function (callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/css/GrafikaApp.Animation.List.css'
-        ]),
-        concat('GrafikaApp.Bundle.Animation.List.min.css'),
-        cleanCSS(),
-        gulp.dest('Grafika.WebSite/wwwroot/css')
-    ], callback);
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/css/GrafikaApp.Animation.List.css'
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Animation.List.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/css'));
 });
 
 gulp.task('min:css:GrafikaApp.Bundle.Animation.Detail', function(callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/css/GrafikaApp.Animation.Detail.css'
-        ]),
-        concat('GrafikaApp.Bundle.Animation.Detail.min.css'),
-        cleanCSS(),
-        gulp.dest('Grafika.WebSite/wwwroot/css')
-    ], callback);
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/css/GrafikaApp.Animation.Detail.css'
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Animation.Detail.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/css'));
 });
 
 gulp.task('min:css:GrafikaApp.Bundle.Drawing', function(callback) {
-    pump([
-        gulp.src([
-            'Grafika.WebSite/wwwroot/css/angular-material.css',
-            'Grafika.WebSite/wwwroot/css/GrafikaApp.Drawing.css'
-        ]),
-        concat('GrafikaApp.Bundle.Drawing.min.css'),
-        cleanCSS(),
-        gulp.dest('Grafika.WebSite/wwwroot/css')
-    ], callback);
+    return gulp.src([
+        'Grafika.WebSite/wwwroot/css/angular-material.css',
+        'Grafika.WebSite/wwwroot/css/GrafikaApp.Drawing.css'
+    ])
+    .pipe(concat('GrafikaApp.Bundle.Drawing.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('Grafika.WebSite/wwwroot/css'));
 });
