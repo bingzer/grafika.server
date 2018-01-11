a325db8fbe1a284cf4540c96af95e88998b9f870
Ricky <ricky@bingzer.com>
2018-01-10 21:35:08 -0500

    Update stickdraw page

---
+++ Grafika.Web/Controllers/Api/StickDrawApiController.cs
@@ -0,0 +1,17 @@
+﻿using Grafika.Configurations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grafika.Web.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/stickdraw")]
+    public class StickDrawApiController : Controller
+    {
+    }
+}
